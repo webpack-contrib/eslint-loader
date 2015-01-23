ac59f61304cc922617476f9db6111e906ae85f9e
Jerry Su <jerry@remind101.com>
2015-01-23 06:58:06 -0800

    Load eslint config from `.eslintrc` or `package.json`

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 "use strict";
 
-var assign = require("object-assign")
 var eslint = require("eslint").linter
-var eslintConfig = require("eslint/conf/eslint.json")
+var Config = require("eslint/lib/config");
 // var loaderUtils = require("loader-utils")
 //
 /**
@@ -14,12 +13,6 @@ var eslintConfig = require("eslint/conf/eslint.json")
  */
 function lint(input, config, webpack) {
   var messages = []
-
-  // merge default config + user config
-  config = {
-    env: assign({}, eslintConfig.env, config.env),
-    rules: assign({}, eslintConfig.rules, config.rules)
-  }
 
   eslint.verify(input, config).forEach(function eslintMessageCallback(m) {
     messages.push(
@@ -45,61 +38,6 @@ function lint(input, config, webpack) {
 }
 
 /**
- * configuration loader
- *
- * @param {Object}   webpack
- * @param {Function} callback
- */
-function loadRcConfig(webpack, callback) {
-  var fs = require("fs")
-  var RcLoader = require("rcloader")
-  var rcLoader = new RcLoader(".eslintrc", null, {
-    loader: function(path) { return path }
-  })
-  var stripJsonComments = require("strip-json-comments")
-
-  if (typeof callback !== "function") {
-    var path = rcLoader.for(this.resourcePath)
-    if (typeof path !== "string") {
-      // no .rc found
-      return {}
-    }
-    else {
-      webpack.addDependency(path)
-      var file = fs.readFileSync(path, "utf8")
-      return JSON.parse(stripJsonComments(file))
-    }
-  }
-  else {
-    rcLoader.for(webpack.resourcePath, function rcLoaderCallback(err, path) {
-      if (err) {
-        throw err
-      }
-
-      if (typeof path !== "string") {
-        // no .rc found
-        return callback(null, {})
-      }
-
-      webpack.addDependency(path)
-      fs.readFile(path, "utf8", function loadRcConfigFileCallback(err, file) {
-        var options
-
-        if (!err) {
-          try {
-            options = JSON.parse(stripJsonComments(file))
-          }
-          catch (e) {
-            err = e
-          }
-        }
-        callback(err, options)
-      })
-    })
-  }
-}
-
-/**
  * webpack loader
  *
  * @param  {String|Buffer} input
@@ -109,27 +47,9 @@ module.exports = function(input) {
   this.options.eslint = this.options.eslint || {}
 
   this.cacheable()
-  var callback = this.async()
 
-    // sync
-  if (!callback) {
-    var config = loadRcConfig(this)
-    lint(input, config, this)
-    return input
-  }
-
-  // async
-  loadRcConfig(this, function loadRcConfigCallback(err, config) {
-    if (err) {
-      return callback(err)
-    }
-
-    try {
-      lint(input, config, this)
-    }
-    catch (e) {
-      return callback(e)
-    }
-    callback(null, input)
-  }.bind(this))
+  // sync
+  var config = new Config(this.options.eslint).getConfig()
+  lint(input, config, this)
+  return input
 }
