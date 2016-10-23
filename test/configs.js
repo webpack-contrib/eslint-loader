ae0908d8d8dac502709953885c6f0b9885821b80
Jay Thomas <jay@gfax.ch>
2016-10-23 15:53:36 -0400

    add tests for custom config paths

---
+++ test/configs.js
@@ -0,0 +1,87 @@
+var test = require("tape")
+var webpack = require("webpack")
+var assign = require("object-assign")
+var conf = require("./utils/conf")
+
+test("eslint-loader should respect custom configFile paths", function(t) {
+  webpack(assign({},
+    conf,
+    {
+      eslint: {
+        ignore: false,
+      },
+      entry: [
+        "./test/fixtures/good.js",
+      ],
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            loader: "./index?{ configFile: './test/utils/eslint-config-quotes-single' }",
+            exclude: /node_modules/,
+          },
+        ],
+      },
+    }
+  ),
+  function(err, stats) {
+    if (err) {
+      throw err
+    }
+
+    t.ok(
+      stats.compilation.warnings.length === 1,
+      "one warning for double quotes"
+    )
+
+    t.notOk(
+      stats.compilation.warnings.length !== 1,
+      "the config file was loaded and double quotes threw a warning"
+    )
+    t.end()
+  })
+})
+
+test("eslint-loader should handle multiple configFile paths", function(t) {
+  webpack(assign({},
+    conf,
+    {
+      eslint: {
+        ignore: false,
+      },
+      entry: [
+        "./test/fixtures/good.js",
+      ],
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            loader: "./index?{ configFile: './test/utils/eslint-config-quotes-single' }",
+            exclude: /node_modules/,
+          },
+          {
+            test: /\.js$/,
+            loader: "./index?{ configFile: './test/utils/eslint-config-semi' }",
+            exclude: /node_modules/,
+          },
+        ],
+      },
+    }
+  ),
+  function(err, stats) {
+    if (err) {
+      throw err
+    }
+
+    t.ok(
+      stats.compilation.warnings.length === 2,
+      "one warning from each config file"
+    )
+
+    t.notOk(
+      stats.compilation.warnings.length !== 2,
+      "all config files was loaded and each threw their warnings"
+    )
+    t.end()
+  })
+})
