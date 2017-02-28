e33ea6bdf6eae683b27f71287d416da7b4005580
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-02-28 23:43:14 +0700

    use loader-fs-cache as the caching engine

--- test/cache.js
+++ test/cache.js
@@ -2,47 +2,183 @@ var test = require("ava")
 var webpack = require("webpack")
 var conf = require("./utils/conf")
 var fs = require("fs")
+var rimraf = require("rimraf")
 
-var cacheFilePath = "./node_modules/.cache/eslint-loader/data.json"
+var cacheDirectory = "./node_modules/.cache/eslint-loader/"
 
-test.cb("eslint-loader can cache results", function(t) {
+test.cb("should output files to cache directory", function(t) {
   t.plan(2)
-  webpack(conf(
-    {
-      entry: "./test/fixtures/cache.js",
-    },
-    {
-      cache: true,
-    }
-  ),
-  function(err) {
-    if (err) {
-      throw err
-    }
-
-    fs.readFile(cacheFilePath, "utf8", function(err, contents) {
+  webpack(
+    conf(
+      {
+        entry: "./test/fixtures/cache.js",
+      },
+      {
+        cache: true,
+      }
+    ),
+    function(err) {
       if (err) {
-        t.fail("expected cache file to have been created")
+        throw err
       }
-      else {
-        t.pass("cache file has been created")
+      fs.readdir(cacheDirectory, (err, files) => {
+        //eslint-disable-next-line
+        console.log(files);
+        t.is(err, null)
+        t.true(files.length === 1)
+        t.end()
+      })
+    }
+  )
+})
 
-        var contentsJson = JSON.parse(contents)
-        t.deepEqual(
-          Object.keys(contentsJson["test/fixtures/cache.js"]),
-          ["hash", "rules", "res"],
-          "cache values have been set for the linted file"
-        )
+test.cb(
+  "should output json.gz files to standard cache dir by default",
+  function(t) {
+    t.plan(2)
+    webpack(
+      conf(
+        {
+          entry: "./test/fixtures/cache.js",
+        },
+        {
+          cache: true,
+        }
+      ),
+      function(err) {
+        if (err) {
+          throw err
+        }
+        fs.readdir(cacheDirectory, (err, files) => {
+          t.is(err, null)
+          t.true(
+            files.filter(file => file.endsWith(".json.gz")).length ===
+              files.length
+          )
+          t.end()
+        })
       }
-
-      t.end()
-
-    })
-
-  })
+    )
+  }
+)
+test.cb(
+  "should output files to standard cache dir if set to true in query",
+  function(t) {
+    t.plan(2)
+    webpack(
+      conf(
+        {
+          entry: "./test/fixtures/cache.js",
+        },
+        {
+          cache: true,
+        }
+      ),
+      function(err) {
+        if (err) {
+          throw err
+        }
+        fs.readdir(cacheDirectory, (err, files) => {
+          t.is(err, null)
+          t.true(files.length === 1)
+          t.end()
+        })
+      }
+    )
+  }
+)
+test.cb("should read from cache directory if cached file exists", function(t) {
+  t.plan(2)
+  webpack(
+    conf(
+      {
+        entry: "./test/fixtures/cache.js",
+      },
+      {
+        cache: true,
+      }
+    ),
+    function(err) {
+      if (err) {
+        throw err
+      }
+    }
+  )
+})
+test.cb("should have one file per module", function(t) {
+  t.plan(2)
+  webpack(
+    conf(
+      {
+        entry: "./test/fixtures/cache.js",
+      },
+      {
+        cache: true,
+      }
+    ),
+    function(err) {
+      if (err) {
+        throw err
+      }
+    }
+  )
+})
+test.cb("should generate a new file if the identifier changes", function(t) {
+  t.plan(2)
+  webpack(
+    conf(
+      {
+        entry: "./test/fixtures/cache.js",
+      },
+      {
+        cache: true,
+      }
+    ),
+    function(err) {
+      if (err) {
+        throw err
+      }
+    }
+  )
+})
+test.cb("should allow to specify the .babelrc file", function(t) {
+  t.plan(2)
+  webpack(
+    conf(
+      {
+        entry: "./test/fixtures/cache.js",
+      },
+      {
+        cache: true,
+      }
+    ),
+    function(err) {
+      if (err) {
+        throw err
+      }
+    }
+  )
+})
+test.cb("can cache results", function(t) {
+  t.plan(2)
+  webpack(
+    conf(
+      {
+        entry: "./test/fixtures/cache.js",
+      },
+      {
+        cache: true,
+      }
+    ),
+    function(err) {
+      if (err) {
+        throw err
+      }
+    }
+  )
 })
 
 // delete the cache file once tests have completed
 test.after.always("teardown", function() {
-  fs.unlinkSync(cacheFilePath)
+  rimraf.sync(cacheDirectory)
 })
