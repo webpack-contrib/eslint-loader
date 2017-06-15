ff4737c82d94b2fdfa91765a4cf75e1babc8eb6b
Đinh Quang Trung <trungdq88@gmail.com>
2017-06-15 11:40:44 +0700

    Added: eslintPath option (#181)
    * Add eslintPath option
    
    * Add test case
    
    * Documentation
    
    * Fix

---
+++ test/eslint-path.js
@@ -0,0 +1,33 @@
+var path = require("path")
+var test = require("ava")
+var webpack = require("webpack")
+var conf = require("./utils/conf")
+
+test.cb("eslint-loader can use another instance of eslint via " +
+  "eslintPath config", function(t) {
+  t.plan(2)
+  webpack(conf(
+    {
+      entry: "./test/fixtures/good.js",
+    },
+    {
+      eslintPath: path.join(__dirname, "mock/eslint-mock.js"),
+    }
+  ),
+  function(err, stats) {
+    if (err) {
+      throw err
+    }
+
+    // console.log(stats.compilation.errors)
+    t.true(
+      stats.hasErrors(),
+      "a file that does not contains error but mock eslint instance " +
+      "returned error"
+    )
+    t.true(
+      stats.compilation.errors[0].message.indexOf("Fake error") > -1
+    )
+    t.end()
+  })
+})
