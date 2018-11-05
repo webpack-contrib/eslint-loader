webpack-contrib/eslint-loader

commit 704024864f737a487ec0b9f9c9b00b11881f2558

Stop fixing loop (#244)
* Stop Fixing if there are no fixable errors or warnings

* Update README.md

* Added Tests for infinte loop

* Fix Typo

index.js
@@ -70,7 +70,7 @@ function printLinterOutput(res, config, webpack) {
     }
 
     // if enabled, use eslint auto-fixing where possible
-    if (config.fix && res.results[0].output) {
+    if (config.fix && (res.results[0].fixableErrorCount > 0 || res.results[0].fixableWarningCount)) {
       var eslint = require(config.eslintPath);
       eslint.CLIEngine.outputFixes(res);
     }

test/autofix-stop.js
@@ -1,0 +1,56 @@
+var fs = require("fs");
+
+var test = require("ava");
+var webpack = require("webpack");
+
+var conf = require("./utils/conf");
+
+var changed = false;
+
+// clone the "fixable" file, so that we do not lose the original contents
+// when the fixes are applied to disk
+test.before(function() {
+  fs.createReadStream("./test/fixtures/nonfixable.js")
+    .pipe(fs.createWriteStream("./test/fixtures/nonfixable-clone.js"))
+    .on("close", function() {
+      fs.watch("./test/fixtures/nonfixable-clone.js", function() {
+        changed = true;
+      });
+    });
+});
+
+test.cb("loader shouldn't change file if there are no fixable errors/warnings", function(
+  t
+) {
+  t.plan(1);
+  webpack(
+    conf({
+      entry: "./test/fixtures/nonfixable-clone.js",
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            use: "./index?fix=true",
+            exclude: /node_modules/
+          }
+        ]
+      }
+    }),
+    function(err) {
+      if (err) {
+        throw err;
+      }
+      // console.log(stats.compilation.errors)
+      t.false(
+        changed,
+        "should not output to file again (triggering a recompile)"
+      );
+      t.end();
+    }
+  );
+});
+
+// remove the clone
+test.after.always(function() {
+  fs.unlinkSync("./test/fixtures/nonfixable-clone.js");
+});

test/fixtures/nonfixable.js
@@ -1,0 +1,5 @@
+function foo() {
+  return stuff;
+}
+
+foo();

commit 276e64f39ccf73f6981c3236bd0005ce2feae7dd

Fix autofix bug introduced in #244

index.js
@@ -70,7 +70,12 @@ function printLinterOutput(res, config, webpack) {
     }
 
     // if enabled, use eslint auto-fixing where possible
-    if (config.fix && (res.results[0].fixableErrorCount > 0 || res.results[0].fixableWarningCount)) {
+    if (
+      config.fix &&
+      res.results[0].output &&
+      res.results[0].errorCount === 0 &&
+      res.results[0].warningCount === 0
+    ) {
       var eslint = require(config.eslintPath);
       eslint.CLIEngine.outputFixes(res);
     }