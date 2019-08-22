c929da7a5a11a0d46f2758d2eed9f9ab7ff191cb
Ricardo Gobbo de Souza <ricardo@datalogix.com.br>
2019-08-22 15:25:44 -0300

    test: close watcher

--- test/autofix-stop.test.js
+++ test/autofix-stop.test.js
@@ -7,19 +7,21 @@ import conf from './utils/conf';
 describe('autofix stop', () => {
   const entry = './test/fixtures/nonfixable-clone.js';
   let changed = false;
+  let watcher;
 
   beforeAll(() => {
     fs.copyFileSync('./test/fixtures/nonfixable.js', entry);
-    fs.watch(entry, {}, (eventType, filename) => {
+    watcher = fs.watch(entry, (eventType, filename) => {
       changed = eventType === 'change' && filename;
     });
   });
 
   afterAll(() => {
+    watcher.close();
     fs.unlinkSync(entry);
   });
 
-  it.skip('should not change file if there are no fixable errors/warnings', (done) => {
+  it('should not change file if there are no fixable errors/warnings', (done) => {
     const compiler = webpack(
       conf(
         {
